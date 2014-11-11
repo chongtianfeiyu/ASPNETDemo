1a155310568a231c3c648fb16dbe8fefa8e9a8e8
suizhikuo <suizhikuo@126.com>
2014-11-11 20:39:33 +0800

    ..

---
+++ Common/JsonHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Common
+{
+    public class JsonHelper
+    {
+        /// <summary>
+        /// 返回json字符串结果
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static string ToJson(this object model)
+        {
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            return jss.Serialize(model);
+        }
+    }
+}
